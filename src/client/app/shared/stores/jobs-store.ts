356ad5f3008c8fa07dcc6b74a08967d39553b161
Remify <remi.bouguermouh@univ-grenoble-alpes.fr>
2017-03-20 17:08:33 +0100

    Update polling on jobstore
    20sec polling

--- src/client/app/shared/stores/jobs-store.ts
+++ src/client/app/shared/stores/jobs-store.ts
@@ -1,10 +1,8 @@
-
-
 import {Injectable} from '@angular/core';
-import { OarApiService }  from "../oar-api/oar-api.service";
+import {OarApiService}  from "../oar-api/oar-api.service";
 import {Job} from "../oar-api/model/job";
 import {List} from 'immutable';
-import {BehaviorSubject} from "rxjs/Rx";
+import {BehaviorSubject, Observable} from "rxjs/Rx";
 
 /**
  *     more about info about stores in Angular2 : https://dzone.com/articles/how-to-build-angular-2-apps-using-observable-data-1
@@ -12,73 +10,96 @@ import {BehaviorSubject} from "rxjs/Rx";
 @Injectable()
 export class JobsStore {
 
-    private _jobs: BehaviorSubject<List<Job>> = new BehaviorSubject(List ([]));
+  private _jobs: BehaviorSubject<List<Job>> = new BehaviorSubject(List([]));
 
-    constructor(private jobOarApiService: OarApiService) {
-        //this.loadInitialData();
+  constructor(private jobOarApiService: OarApiService) {
+    //this.loadInitialData();
+
+    Observable.interval(20000)
+      .mergeMap(() => this.jobOarApiService.getJobs())
+      .subscribe(
+        json => this.initJobList(json)
+      )
+  }
+
+  /**
+   * load from userspace ?
+   */
+  loadInitialData() {
+
+  }
+
+  initJobList(json: any) {
+
+    // Update if there's anything to update
+    if(json.items > 0) {
+
+
+      let newJobs: List<Job> = List([]);
+
+      json.items.forEach(function (jsonJob: any) {
+        let job: Job = new Job().deserialize(jsonJob)
+        newJobs.push(job)
+      })
+
+      this._jobs.next(newJobs);
+
+    }
+  }
+
+  addJob(id: string) {
+
+    let obs = this.jobOarApiService.getJob(id);
+
+    obs.subscribe(
+      json => this.addJobWithJob(
+        new Job().deserialize(json),
+        this._jobs.getValue().toArray()
+      ),
+      error => console.log(error)
+    );
+
+    return true
+  }
+
+  /**
+   * todo : return value ?
+   */
+  addJobWithJob(job: Job, arr: Array<Job>) {
+
+    if (!this.containsJob(job.id.toString(), arr)) {
+      this._jobs.next(this._jobs.getValue().push(job));
     }
 
-    /**
-     * load from userspace ?
-     */
-    loadInitialData() {
+    return this._jobs;
+  }
 
-    }
+  /**
+   * Return the Job List as an Observable
+   */
+  get jobs() :Observable<List<Job>>{
+    return this._jobs.asObservable();
+  }
 
-    addJob(id:string) {
+  /**
+   * return a Job given an id
+   *
+   * todo : if job doesn't exist, do a request
+   */
+  getJob(id: string): Job {
+    return this._jobs.getValue().find(
+      (job) => job.id.toString() === id
+    )
+  }
 
-        let obs = this.jobOarApiService.getJob(id);
-
-        obs.subscribe(
-            json =>  this.addJobWithJob(
-                new Job().deserialize(json),
-                this._jobs.getValue().toArray()
-                ),
-            error => console.log(error)
-        );
-
-        return true
-    }
-
-    /**
-     * todo : return value ?
-     */
-    addJobWithJob(job:Job, arr :Array<Job>) {
-
-        if(! this.containsJob(job.id.toString(), arr)) {
-            this._jobs.next(this._jobs.getValue().push(job));
-        }
-
-        return this._jobs;
-    }
-
-    /**
-     * Return the Job List as an Observable
-     */
-    get jobs() {
-        return this._jobs.asObservable();
-    }
-
-    /**
-     * return a Job given an id
-     *
-     * todo : if job doesn't exist, do a request
-     */
-    getJob(id:string): Job {
-        return this._jobs.getValue().find(
-            (job) => job.id.toString() === id
-        )
-    }
-
-    /**
-     * Check if a job exist in the List
-     */
-    containsJob(id:string, arr :Array<Job>):boolean {
-
-       return arr.some(
-           (job) => job.id.toString() === id
-       )
-    }
+  /**
+   * Check if a job exist in the List
+   */
+  containsJob(id: string, arr: Array<Job>): boolean {
+    return arr.some(
+      (job) => job.id.toString() === id
+    )
+  }
 
 
 }
