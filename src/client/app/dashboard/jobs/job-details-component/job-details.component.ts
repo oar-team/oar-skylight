9770137e394732977e2b1804fed92acdcd951096
Remify <remi.bouguermouh@univ-grenoble-alpes.fr>
2017-03-30 14:44:10 +0200

    Fixe bug new job not reconize

--- src/client/app/dashboard/jobs/job-details-component/job-details.component.ts
+++ src/client/app/dashboard/jobs/job-details-component/job-details.component.ts
@@ -1,12 +1,12 @@
-import {Component, ViewChild} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
-import {OarApiService} from '../../../shared/oar-api/oar-api.service';
-import {JobsStore} from '../../../shared/stores/jobs-store';
-import {Job} from '../../../shared/oar-api/model/job';
-import {UserConfigStore} from '../../../shared/stores/user-config-store';
-import {Link} from '../../../shared/oar-api/model/link';
-import {Observable} from "rxjs";
-import {Location} from "@angular/common";
+import { Component, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OarApiService } from '../../../shared/oar-api/oar-api.service';
+import { JobsStore } from '../../../shared/stores/jobs-store';
+import { Job } from '../../../shared/oar-api/model/job';
+import { UserConfigStore } from '../../../shared/stores/user-config-store';
+import { Link } from '../../../shared/oar-api/model/link';
+import { Observable } from "rxjs";
+import { Location } from "@angular/common";
 
 @Component({
   moduleId: module.id,
@@ -29,11 +29,11 @@ export class JobDetails {
   jobKeys: String[];
 
   constructor(private route: ActivatedRoute,
-              private router: Router,
-              private apiService: OarApiService,
-              private jobStore: JobsStore,
-              private userConfig: UserConfigStore,
-              private location: Location) {
+    private router: Router,
+    private apiService: OarApiService,
+    private jobStore: JobsStore,
+    private userConfig: UserConfigStore,
+    private location: Location) {
     this.job = new Job();
     this.buttonState = 1;
     this.jobKeys = [];
@@ -49,34 +49,16 @@ export class JobDetails {
     this.route.params.subscribe(params => {
       let id = +params['id'];// (+) converts string 'id' to a number
       this.id = id;
+
       this.jobStore.jobs.subscribe(
-        list => this.job = list.find( job => job.id == id)
+        list => {
+          console.log(list.find(job => job.id == id))
+          this.job = list.find(job => job.id == id)
+
+          this.jobKeys = Object.keys(this.job.json);
+          this.jobKeys.sort();
+        }
       );
-
-      /**
-       * Hack for the case of new job not yet available
-       */
-      if (!this.job.json) {
-
-        this.getJob(id.toString()).subscribe(
-          () => {
-
-            // Sort on json keys (for clarity)
-            this.jobKeys = Object.keys(this.job.json);
-            this.jobKeys.sort();
-
-          }
-        )
-
-      } else {
-
-
-        // Sort on json keys (for clarity)
-        this.jobKeys = Object.keys(this.job.json);
-        this.jobKeys.sort();
-
-
-      }
 
     });
 
@@ -85,28 +67,28 @@ export class JobDetails {
 
   /**
    *    Get the json of a given job id
-   */
-  getJob(id: string): Observable<any> {
-    let jsonJob: any;
+  //  */
+  // getJob(id: string): Observable<any> {
+  //   let jsonJob: any;
 
-    let obs = this.apiService.getJob(id)
+  //   let obs = this.apiService.getJob(id)
 
-    obs.subscribe(
-      data => this.setJob(data),
-      error => console.log(error)
-    )
+  //   obs.subscribe(
+  //     data => this.setJob(data),
+  //     error => console.log(error)
+  //   )
 
-    return obs
-  }
+  //   return obs
+  // }
 
   /**
    *  Populate job with json
    */
-  setJob(json: any) {
-    console.log(json);
-    this.job = new Job().deserialize(json);
-    console.log(this.job);
-  }
+  // setJob(json: any) {
+  //   console.log(json);
+  //   this.job = new Job().deserialize(json);
+  //   console.log(this.job);
+  // }
 
   getDetails() {
 
@@ -114,7 +96,6 @@ export class JobDetails {
       this.buttonState = 1;
       this.messageButton = "Display Details";
       this.jobDetailsTable.nativeElement.style = "display:none;"
-      this.getJob(this.id.toString());
     } else {
       this.buttonState = 0;
       this.messageButton = "Hide Details";
