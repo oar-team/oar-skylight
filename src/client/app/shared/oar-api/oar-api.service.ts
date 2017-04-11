f0c84fe78e7a48548212f33510d5b41dcf93294a
Remify <remi.bouguermouh@univ-grenoble-alpes.fr>
2017-04-11 14:27:19 +0200

    lint

--- src/client/app/shared/oar-api/oar-api.service.ts
+++ src/client/app/shared/oar-api/oar-api.service.ts
@@ -1,8 +1,8 @@
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import { Injectable }     from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Job } from '../../shared/oar-api/model/job';
-import { Config } from "../config/env.config";
+import { Config } from '../config/env.config';
 import 'rxjs/add/operator/map';
 
 /**
@@ -14,34 +14,35 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class OarApiService {
 
-    constructor(
-        private http: Http
-    ){}
-
-
     private login = 'docker';
     private mdp = 'docker';
     private baseLogin = Config.API_PROTOCOLE + '://' + this.login + ':' + this.mdp + '@';
 
     private baseUrlOar = Config.API + 'oarapi-priv/';
-    private urlResources =  this.baseLogin + this.baseUrlOar + 'resources.json';
+    private urlResources = this.baseLogin + this.baseUrlOar + 'resources.json';
     private urlJobs = this.baseLogin + this.baseUrlOar + 'jobs.json';
     private _urlMedia = this.baseLogin + this.baseUrlOar + 'media';
 
-  /**
-     *     Get all current jobs
-     *     __return format :__ JSON
-     */
-    getJobs()  :Observable<Response>{
+    constructor(
+        private http: Http
+    ) { }
+
+
+
+    /**
+       *     Get all current jobs
+       *     __return format :__ JSON
+       */
+    getJobs(): Observable<Response> {
         return this.http.get(
             this.urlJobs
         ).map(res => res.json());
     }
-    
+
     /**
      * Return a JSON of given job by id
      */
-    getJobsById(ids :number[]) :Observable<Response>{
+    getJobsById(ids: number[]): Observable<Response> {
         return this.http.get(
             this.baseLogin + this.baseUrlOar + 'jobs.json?ids=' + ids.toString()
         ).map(res => res.json());
@@ -51,7 +52,7 @@ export class OarApiService {
     *    Get a job by its id
     *    __return format :__ JSON
     */
-    getJob(id: string)  :Observable<Response>{
+    getJob(id: string): Observable<Response> {
         return this.http.get(
             this.baseLogin + this.baseUrlOar + 'jobs/' + id + '.json'
         ).map(res => res.json());
@@ -60,11 +61,11 @@ export class OarApiService {
     /**
      * Get the jobs of a given username
      */
-    getUserJobs(username: string)  :Observable<Response>{
+    getUserJobs(username: string): Observable<Response> {
         return this.http.get(
 
             //    TODO : Parameters for states
-            this.urlJobs + "?owner=" + username + "&state=Terminated,Running,Waiting"
+            this.urlJobs + '?owner=' + username + '&state=Terminated,Running,Waiting'
         ).map(res => res.json());
 
     }
@@ -74,8 +75,8 @@ export class OarApiService {
      *     Get all resources
      *     __return format :__ JSON
      */
-    getResources()  :Observable<Response>{
-          return this.http.get(
+    getResources(): Observable<Response> {
+        return this.http.get(
             this.urlResources
         ).map(res => res.json());
     }
@@ -84,11 +85,11 @@ export class OarApiService {
     /**
      * TEST
      */
-    getMedia(path :string)  :Observable<Response>{
+    getMedia(path: string): Observable<Response> {
 
         let urlStd = 'http://localhost:46668/oarapi-priv/media/~/' + path;
 
-         let headers = new Headers();
+        let headers = new Headers();
         headers.append('Content-Type', 'text/html');
 
         return this.http.get(
@@ -98,32 +99,40 @@ export class OarApiService {
 
     }
 
-  /**
-   * Getter for urlMedia
-   * @returns {string}
-   */
+    /**
+     * Getter for urlMedia
+     * @returns {string}
+     */
     get urlMedia(): string {
-      return this._urlMedia;
+        return this._urlMedia;
     }
 
-    postNewJob(resource :string, name :string, command:string, directory:string, property:string, type :string, reservation:string) :Observable<Response> {
-      let job = {
-        'resource': resource,
-        'name': name,
-        'command': command,
-        'directory': directory,
-        'property': property,
-        'type': type,
-        'reservation': reservation,
-      };
+    postNewJob(
+        resource: string,
+        name: string,
+        command: string,
+        directory: string,
+        property: string,
+        type: string,
+        reservation: string
+    ): Observable<Response> {
+        let job = {
+            'resource': resource,
+            'name': name,
+            'command': command,
+            'directory': directory,
+            'property': property,
+            'type': type,
+            'reservation': reservation,
+        };
 
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
 
-      return this.http.post(
-          this.baseLogin + this.baseUrlOar + "jobs",
-        JSON.stringify(job), { headers: headers }
-      )
+        return this.http.post(
+            this.baseLogin + this.baseUrlOar + 'jobs',
+            JSON.stringify(job), { headers: headers }
+        )
 
     }
 
