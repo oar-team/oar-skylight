ac99b1c1520259011b9e27f0ce4fc46200be9633
Remify <remi.bouguermouh@univ-grenoble-alpes.fr>
2017-05-04 14:48:23 +0200

    demo gricad

--- src/client/app/dashboard/forms/post-job-component/new-job-form.component.ts
+++ src/client/app/dashboard/forms/post-job-component/new-job-form.component.ts
@@ -59,4 +59,57 @@ export class NewJobFormComponent {
     }
   }
 
+  demo() :void {
+      let jobsToPost :JobFormTemplate[] = [];
+      
+
+        let jobT = new JobFormTemplate();
+        jobT.script = `/bin/sleep 5; 
+        echo 'Hello World \n Job took 5s';
+        `;
+        jobsToPost.push(jobT);
+
+      for(let i = 0; i < 32 ; i++) {
+        let jobT = new JobFormTemplate();
+        let randomTime = Math.floor(Math.random() * (150 - 5)) + 5;
+        jobT.name = 'Demo job ' + i;
+        jobT.script = '/bin/sleep ' + randomTime;
+        jobsToPost.push(jobT);
+      }
+
+      jobsToPost.forEach(
+        job =>  {
+          this.jobTemplate = job;
+          this.submitNewJobFormDemo();
+        }
+      );
+
+      this.router.navigate(['dashboard/jobs']);
+
+  }
+
+    submitNewJobFormDemo() {
+    let resource = this.jobTemplate.getResource();
+    let name = this.jobTemplate.getName();
+    let command = this.jobTemplate.getCommand();
+    let directory = this.jobTemplate.getDirectory();
+    let property = this.jobTemplate.getProperty();
+    let type = this.jobTemplate.getType();
+    let reservation = this.jobTemplate.getReservation();
+
+    this.oarApi.postNewJob(resource, name, command, directory, property, type, reservation).subscribe(
+      result => {
+        let jobId = result.json().id
+        if (jobId) {
+          this.jobStore.addJob(result.json().id).subscribe(
+            //() => this.redirect(result.json()),
+            err => console.log(err),
+          )
+        }
+      },
+      err => console.log(err)
+    )
+  }
+
+
 }
