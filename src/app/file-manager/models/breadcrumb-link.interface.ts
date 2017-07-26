68093125dd0c4088ebcf0c3ae373247139b93f03
Remify <remibouguermouh@gmail.com>
2017-07-26 18:02:58 +0200

    doc: BreadcrumbLink Interface documentation

--- src/app/file-manager/models/breadcrumb-link.interface.ts
+++ src/app/file-manager/models/breadcrumb-link.interface.ts
@@ -1,10 +1,25 @@
 /**
- * 
+ * Represente a link.
  * 
  * @export
  * @interface BreadcrumbLink
  */
 export interface BreadcrumbLink {
+  /**
+   *  display name 
+   * 
+   * @type {string}
+   * @memberof BreadcrumbLink
+   */
   name: string;
+
+  /**
+   * URI link
+   * 
+   * @example 
+   * /myfolder/
+   * @type {string}
+   * @memberof BreadcrumbLink
+   */
   link: string;
 }
