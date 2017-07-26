15abfc8e332d55a102be3effe69f21a2c6acc575
Remify <remibouguermouh@gmail.com>
2017-07-26 18:03:12 +0200

    doc: FmItem Interface documentation

--- src/app/file-manager/models/fm-item.interface.ts
+++ src/app/file-manager/models/fm-item.interface.ts
@@ -1,14 +1,47 @@
 /**
  * Interface for a remote item. This item can be a file or a directory (type property)
- * type value is either 'f' or 'd'
+ * 
  * 
  * @export
  * @interface FmItem
  */
 export interface FmItem {
+  /**
+   * Display name
+   * 
+   * @type {string}
+   * @memberof FmItem
+   */
   name: string;
+  /**
+   * Timestamp
+   * 
+   * @type {number}
+   * @memberof FmItem
+   */
   mtime: number;
+  /**
+   * Type of item : Folder or Directory
+   * 
+   * type value is either 'f' or 'd'
+   * 
+   * @type {string}
+   * @memberof FmItem
+   */
   type: string;
+
+  /**
+   * Unix mode
+   * 
+   * @type {number}
+   * @memberof FmItem
+   */
   mode: number;
+  /**
+   * octet size
+   * 
+   * @type {number}
+   * @memberof FmItem
+   */
   size?: number;
 }
